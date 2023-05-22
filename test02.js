4a6c9b043bd9768f64ac236bdf450e301f6350ee
mkhajiit <132371050+mkhajiit@users.noreply.github.com>
2023-05-22 19:59:20 +0900

    코딩테스트 풀이

---
+++ test02.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    console.log('a = '+ parseInt(input[0]));
+    console.log('b = '+ parseInt(input[1]));
+});
